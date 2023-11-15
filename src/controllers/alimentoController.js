1d917dfa3e0fc09d371c0e1a1be69abe1eeecb77
Juanml0331123 <juanmiguelleon43@gmail.com>
2023-11-14 21:32:48 -0500

    Cambio realizado en el controlador de alimento

--- src/controllers/alimentoController.js
+++ src/controllers/alimentoController.js
@@ -4,118 +4,106 @@ const path = require('path');
 const loadSiteStructure = (req, res) => {
   const indexPath = path.join(__dirname, '../public/index.html');
   res.sendFile(indexPath);
-}
+};
 
 const loadSiteStyles = (req, res) => {
   const stylePath = path.join(__dirname, '../public/index.css');
   res.sendFile(stylePath);
-}
+};
 
 const getAllAlimentos = (req, res) => {
-    db.query('SELECT * FROM Alimentos', (err, results) => {
-      if (err) {
-        console.error('Error al consultar la base de datos:', err);
-        res.status(500).json({ error: 'Error en la base de datos' });
-      } else {
-        res.json(results);
-      }
-    });
-  };
+  db.query('SELECT * FROM Alimentos', (err, results) => {
+    if (err) {
+      console.error('Error al consultar la base de datos:', err);
+      res.status(500).json({ error: 'Error en la base de datos' });
+    } else {
+      res.json(results);
+    }
+  });
+};
 
 const getOneAlimento = (req, res) => {
-    const { id } = req.params;
-  
-    db.query('SELECT * FROM Alimentos WHERE ID = ?', [id], (err, results) => {
-      if (err) {
-        console.error('Error al consultar la base de datos:', err);
-        res.status(500).json({ error: 'Error en la base de datos' });
+  const { id } = req.params;
+
+  db.query('SELECT * FROM Alimentos WHERE ID = ?', [id], (err, results) => {
+    if (err) {
+      console.error('Error al consultar la base de datos:', err);
+      res.status(500).json({ error: 'Error en la base de datos' });
+    } else {
+      if (results.length > 0) {
+        res.json(results[0]);
       } else {
-        if (results.length > 0) {
-          res.json(results[0]);
-        } else {
-          res.status(404).json({ error: 'Paciente no encontrado' });
-        }
+        res.status(404).json({ error: 'Paciente no encontrado' });
       }
-    });
-  };
+    }
+  });
+};
 
 const createOneAlimento = (req, res) => {
-    const {
-      ID,
-      Nombre,
-      Descripcion,
-      GrupoAlimenticioID,
-    } = req.body;
-    
-    const sql =
-      'INSERT INTO Alimentos (ID, Nombre, Descripcion, Id_GrupoAlimentario) VALUES (?, ?, ?, ?)';
-  
-    db.query(
-      sql,
-      [
-        ID,
-        Nombre,
-        Descripcion,
-        GrupoAlimenticioID,
-      ],
-      (err, result) => {
-        if (err) {
-          console.error('Error al agregar el alimento:', err);
-          res.status(500).json({ error: 'Error en la base de datos' });
-        } else {
-          res.json({ message: 'Alimento agregado con éxito' });
-        }
-      }
-    );
-  };
+  const { ID, Nombre, Descripcion, GrupoAlimenticioID } = req.body;
 
-  const updateOneAlimento = (req, res) => {
-    const { id } = req.params;
-    const { Nombre, Descripcion } = req.body;
-  
-    const sql = 'UPDATE Alimentos SET Nombre = ?, Descripcion = ? WHERE ID = ?';
-  
-    db.query(sql, [Nombre, Descripcion, id], (err, result) => {
+  const sql =
+    'INSERT INTO Alimentos (ID, Nombre, Descripcion, Id_GrupoAlimentario) VALUES (?, ?, ?, ?)';
+
+  db.query(
+    sql,
+    [ID, Nombre, Descripcion, GrupoAlimenticioID],
+    (err, result) => {
       if (err) {
-        console.error('Error al actualizar el alimento:', err);
+        console.error('Error al agregar el alimento:', err);
         res.status(500).json({ error: 'Error en la base de datos' });
       } else {
-        if (result.affectedRows > 0) {
-          res.json({ message: 'Alimento actualizado con éxito' });
-        } else {
-          res.status(404).json({ error: 'Alimento no encontrado' });
-        }
+        res.json({ message: 'Alimento agregado con éxito' });
       }
-    });
-  };
-  
+    },
+  );
+};
 
+const updateOneAlimento = (req, res) => {
+  const { id } = req.params;
+  const { Nombre, Descripcion } = req.body;
+
+  const sql = 'UPDATE Alimentos SET Nombre = ?, Descripcion = ? WHERE ID = ?';
+
+  db.query(sql, [Nombre, Descripcion, id], (err, result) => {
+    if (err) {
+      console.error('Error al actualizar el alimento:', err);
+      res.status(500).json({ error: 'Error en la base de datos' });
+    } else {
+      if (result.affectedRows > 0) {
+        res.json({ message: 'Alimento actualizado con éxito' });
+      } else {
+        res.status(404).json({ error: 'Alimento no encontrado' });
+      }
+    }
+  });
+};
 
 const deleteOneAlimento = (req, res) => {
-    const { id } = req.params;
-  
-    const sql = 'DELETE FROM Alimentos WHERE ID = ?';
-  
-    db.query(sql, [id], (err, result) => {
-      if (err) {
-        console.error('Error al eliminar el alimento:', err);
-        res.status(500).json({ error: 'Error en la base de datos' });
+  const { id } = req.params;
+
+  const sql = 'DELETE FROM Alimentos WHERE ID = ?';
+
+  db.query(sql, [id], (err, result) => {
+    if (err) {
+      console.error('Error al eliminar el alimento:', err);
+      res.status(500).json({ error: 'Error en la base de datos' });
+    } else {
+      if (result.affectedRows > 0) {
+        res.json({ message: 'Alimento eliminado con éxito' });
       } else {
-        if (result.affectedRows > 0) {
-          res.json({ message: 'Alimento eliminado con éxito' });
-        } else {
-          res.status(404).json({ error: 'Alimento no encontrado' });
-        }
+        res.status(404).json({ error: 'Alimento no encontrado' });
       }
-    });
-  };
+    }
+  });
+};
 
 module.exports = {
-    getAllAlimentos,
-    getOneAlimento,
-    createOneAlimento,
-    updateOneAlimento,
-    deleteOneAlimento,
-    loadSiteStructure,
-    loadSiteStyles,
-  };
+  getAllAlimentos,
+  getOneAlimento,
+  createOneAlimento,
+  updateOneAlimento,
+  deleteOneAlimento,
+  loadSiteStructure,
+  loadSiteStyles,
+};
--- src/v1/routes/pacienteRoutes.js
+++ src/v1/routes/pacienteRoutes.js
@@ -1,20 +1,8 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
 const pacienteController = require('../../controllers/pacienteController');
 
-// Obtener todos los Pacientes
-router.get('/', pacienteController.getAllPacientes);
-
-// Obtener un solo Paciente por su ID
-router.get('/:id', pacienteController.getOnePaciente);
-
-// Agregar un nuevo Paciente
-router.post('/', pacienteController.createOnePaciente);
-
-// Actualizar un Paciente
-router.patch('/:id', pacienteController.updateOnePaciente);
-
-// Eliminar un Paciente
-router.delete('/:id', pacienteController.deleteOnePaciente);
+router.get('/', pacienteController.getOnePaciente);
 
 module.exports = router;
