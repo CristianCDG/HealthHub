f87e9f6daf8526acde38fe64afa906f6628fe969
Juanml0331123 <juanmiguelleon43@gmail.com>
2023-11-19 21:26:16 -0500

    Nada a commitar, diretório de trabalho limpo

--- src/public/adminInterface/scripts/WindowChanger.js
+++ src/public/adminInterface/scripts/WindowChanger.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var listFoodBtn = document.getElementById("listFoodBtn");
   var updateFoodBtn = document.getElementById("updateFoodBtn");
   var insertGroupBtn = document.getElementById("insertFoodGroupBtn");
+  var modificarGrupoBtn = document.getElementById("modificarGrupoBtn");
 
   insertFoodBtn.addEventListener('click', function (event) {
     event.preventDefault();
@@ -39,6 +40,13 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log('El enlace fue clickeado');
     showCreateGroupWindow();
   });
+
+  modificarGrupoBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    // Aquí va tu código para manejar el click
+    console.log('El enlace fue clickeado');
+    showUpdateGroupWindow();
+  });
 });
 
 function showInsertFoodWindow() {
@@ -46,7 +54,11 @@ function showInsertFoodWindow() {
   var delFoodCont = document.getElementById("deleteAlimentoContainer");
   var listFoodCont = document.getElementById("viewAlimentoContainer");
   var updateFoodCont = document.getElementById("actualizarAlimentoContainer");
+  // Grupo Alimentario
   var createGroupCont = document.getElementById("insertarGrupoContainer");
+  var updateGroupCont = document.getElementById("modificarGrupoContainer");
+  var createGroupCont = document.getElementById("insertarGrupoContainer");
+  var updateGroupCont = document.getElementById("modificarGrupoContainer");
 
   if (delFoodCont.classList.contains("showAlimentoContainer")) {
     delFoodCont.classList.remove("showAlimentoContainer");
@@ -64,6 +76,10 @@ function showInsertFoodWindow() {
     createGroupCont.classList.remove("showAlimentoContainer");
   }
 
+  if (updateGroupCont.classList.contains("showAlimentoContainer")) {
+    updateGroupCont.classList.remove("showAlimentoContainer");
+  }
+
   addFoodCont.classList.add("showAlimentoContainer");
 }
 
@@ -73,6 +89,7 @@ function showDeleteFoodWindow() {
   var listFoodCont = document.getElementById("viewAlimentoContainer");
   var updateFoodCont = document.getElementById("actualizarAlimentoContainer");
   var createGroupCont = document.getElementById("insertarGrupoContainer");
+  var updateGroupCont = document.getElementById("modificarGrupoContainer");
 
   if (addFoodCont.classList.contains("showAlimentoContainer")) {
     addFoodCont.classList.remove("showAlimentoContainer");
@@ -88,6 +105,10 @@ function showDeleteFoodWindow() {
 
   if (createGroupCont.classList.contains("showAlimentoContainer")) {
     createGroupCont.classList.remove("showAlimentoContainer");
+  }
+
+  if (updateGroupCont.classList.contains("showAlimentoContainer")) {
+    updateGroupCont.classList.remove("showAlimentoContainer");
   }
 
   delFoodCont.classList.add("showAlimentoContainer");
@@ -100,6 +121,8 @@ function showListFoodWindow() {
   var updateFoodCont = document.getElementById("actualizarAlimentoContainer");
   var createGroupCont = document.getElementById("insertarGrupoContainer");
 
+  var updateGroupCont = document.getElementById("modificarGrupoContainer");
+
   if (addFoodCont.classList.contains("showAlimentoContainer")) {
     addFoodCont.classList.remove("showAlimentoContainer");
   }
@@ -114,6 +137,10 @@ function showListFoodWindow() {
 
   if (createGroupCont.classList.contains("showAlimentoContainer")) {
     createGroupCont.classList.remove("showAlimentoContainer");
+  }
+
+  if (updateGroupCont.classList.contains("showAlimentoContainer")) {
+    updateGroupCont.classList.remove("showAlimentoContainer");
   }
 
   listFoodCont.classList.add("showAlimentoContainer");
@@ -126,6 +153,8 @@ function showUpdateFoodWindow() {
   var addFoodCont = document.getElementById("insertarAlimentoContainer");
   var createGroupCont = document.getElementById("insertarGrupoContainer");
 
+  var updateGroupCont = document.getElementById("modificarGrupoContainer");
+
   if (addFoodCont.classList.contains("showAlimentoContainer")) {
     addFoodCont.classList.remove("showAlimentoContainer");
   }
@@ -142,6 +171,10 @@ function showUpdateFoodWindow() {
     createGroupCont.classList.remove("showAlimentoContainer");
   }
 
+  if (updateGroupCont.classList.contains("showAlimentoContainer")) {
+    updateGroupCont.classList.remove("showAlimentoContainer");
+  }
+
   updateFoodCont.classList.add("showAlimentoContainer");
 }
 
@@ -151,6 +184,8 @@ function showCreateGroupWindow() {
   var listFoodCont = document.getElementById("viewAlimentoContainer");
   var delFoodCont = document.getElementById("deleteAlimentoContainer");
   var addFoodCont = document.getElementById("insertarAlimentoContainer");
+
+  var updateGroupCont = document.getElementById("modificarGrupoContainer");
 
   if (addFoodCont.classList.contains("showAlimentoContainer")) {
     addFoodCont.classList.remove("showAlimentoContainer");
@@ -168,6 +203,40 @@ function showCreateGroupWindow() {
     updateFoodCont.classList.remove("showAlimentoContainer");
   }
 
+  if (updateGroupCont.classList.contains("showAlimentoContainer")) {
+    updateGroupCont.classList.remove("showAlimentoContainer");
+  }
+
   createGroupCont.classList.add("showAlimentoContainer");
 }
 
+function showUpdateGroupWindow() {
+  var updateGroupCont = document.getElementById("modificarGrupoContainer");
+  var createGroupCont = document.getElementById("insertarGrupoContainer");
+  var updateFoodCont = document.getElementById("actualizarAlimentoContainer");
+  var listFoodCont = document.getElementById("viewAlimentoContainer");
+  var delFoodCont = document.getElementById("deleteAlimentoContainer");
+  var addFoodCont = document.getElementById("insertarAlimentoContainer");
+
+  if (addFoodCont.classList.contains("showAlimentoContainer")) {
+    addFoodCont.classList.remove("showAlimentoContainer");
+  }
+
+  if (createGroupCont.classList.contains("showAlimentoContainer")) {
+    createGroupCont.classList.remove("showAlimentoContainer");
+  }
+
+  if (delFoodCont.classList.contains("showAlimentoContainer")) {
+    delFoodCont.classList.remove("showAlimentoContainer");
+  }
+
+  if (listFoodCont.classList.contains("showAlimentoContainer")) {
+    listFoodCont.classList.remove("showAlimentoContainer");
+  }
+
+  if (updateFoodCont.classList.contains("showAlimentoContainer")) {
+    updateFoodCont.classList.remove("showAlimentoContainer");
+  }
+
+  updateGroupCont.classList.add("showAlimentoContainer");
+}
